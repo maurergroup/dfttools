7f126fbc813c5625ef753f0fbff14e1e322789c8
lukashoermann200 <lukashoermann2000@gmail.com>
2024-05-28 13:16:20 +0100

    Re-implemented test geometry in pytest.
    Currently two tests are there:
    - test_save_and_read_file
    - test_get_displacement_of_atoms

--- tests/test_geometry.py
+++ tests/test_geometry.py
@@ -1,31 +1,38 @@
-# import unittest
-# import os
-# from dfttools.geometry import AimsGeometry
+import pytest
+import os
+import numpy as np
+from dfttools.geometry import AimsGeometry
 
 
-# class TestParserAndWriter(unittest.TestCase):
-#     def _get_geometry(self):
-#         geometry = AimsGeometry()
-#         geometry.add_atoms(
-#             cartesian_coords=[
-#                 [0, 0, 0],
-#                 [1, 0, 0],
-#             ],
-#             species=["H", "H"]
-#         )
+#@pytest.fixture
+def get_geometry():
+    geometry = AimsGeometry()
+    geometry.add_atoms(
+        cartesian_coords=[
+            [0, 0, 0],
+            [1, 0, 0],
+        ],
+        species=["H", "H"],
+        constrain_relax=[np.array([False, False, False]),
+                         np.array([True, True, True])]
+    )
 
-#         return geometry
-
-
-#     def test_save_and_read_file(self):
-#         geometry = self._get_geometry()
-#         geometry.save_to_file('temp/geometry.in')
-
-#         geometry_read = AimsGeometry('temp/geometry.in')
-#         os.remove('temp/geometry.in')
-
-#         self.assertTrue(geometry == geometry_read)
+    return geometry
 
 
-# if __name__ == '__main__':
-#     unittest.main()
+def test_save_and_read_file():
+    geometry = get_geometry()
+    geometry.save_to_file('temp/geometry.in')
+
+    geometry_read = AimsGeometry('temp/geometry.in')
+    os.remove('temp/geometry.in')
+
+    assert geometry == geometry_read
+
+
+def test_get_displacement_of_atoms():
+    geometry = get_geometry()
+    geometry_new = geometry.get_displacement_of_atoms(1)
+    
+    assert not np.allclose( geometry.coords[0], geometry_new.coords[0] )
+    assert np.allclose( geometry.coords[1], geometry_new.coords[1] )
